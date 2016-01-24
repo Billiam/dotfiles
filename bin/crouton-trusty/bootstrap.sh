4505aa0aaa4a9e66e5ea71855b840151f65eac93
Billiam <billiamthesecond@gmail.com>
2016-01-24 01:12:54 -0600

    Adding gems, preferences

--- bin/crouton-trusty/bootstrap.sh
+++ bin/crouton-trusty/bootstrap.sh
@@ -13,6 +13,7 @@ sudo add-apt-repository ppa:brightbox/ruby-ng -y
 sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
 sudo aptitude update
 sudo aptitude install ruby2.3 ruby2.3-dev sublime-text-installer ruby-switch
+sudo gem install bundler pry foreman
 
 ln -s /opt/sublime_text/sublime_text /usr/local/bin
 
