14f638dd656a9bf9d0f9064153da94cf59d82cb2
Billiam <billiamthesecond@gmail.com>
2016-01-24 13:29:01 -0600

    Setting locale, install package control

--- bin/crouton-trusty/bootstrap.sh
+++ bin/crouton-trusty/bootstrap.sh
@@ -9,14 +9,20 @@ git config --global user.name Billiam
 
 setxkbmap dvorak
 
+grep -q "en_US.UTF-8" /etc/environment || sudo sh -c "echo 'LC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8' >> /etc/environment"
+
 sudo add-apt-repository ppa:brightbox/ruby-ng -y
 sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
 sudo aptitude update
 sudo aptitude install ruby2.3 ruby2.3-dev sublime-text-installer ruby-switch
 sudo gem install bundler pry foreman
 
-ln -s /opt/sublime_text/sublime_text /usr/local/bin
+sudo ln -s /opt/sublime_text/sublime_text /usr/local/bin
 
-[[ -d ~/dotfiles]] || git clone https://github.com/Billiam/dotfiles ~/dotfiles
+package=~/.config/sublime-text-3/Installed\ Packages/Package\ Control.sublime-package
+
+[[ -f "$package" ]] || curl -o "$package" https://packagecontrol.io/Package%20Control.sublime-package
+
+[[ -d ~/dotfiles ]] || git clone https://github.com/Billiam/dotfiles ~/dotfiles
 
 $(dirname $0)/../../install
